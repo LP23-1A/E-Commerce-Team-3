607a1672aac79df8667567f681fed70d0e7aefca
Sengen-sen <denisabb02@gmail.com>
2024-03-05 12:28:56 +0800

    order

---
+++ backend/src/model/order.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+const orderSchema = new mongoose.Schema({
+  userId: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  orderNumber: Number,
+  foods: [],
+  totalPrice: Number,
+  process: ["order", "complete"],
+  createdDate: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  district: String,
+  khoroo: String,
+  apartment: String,
+});
+const orderModel = mongoose.model("order", orderSchema);
+export { orderModel };
+
+// import mongoose from "mongoose";
+// const orderSchema = new mongoose.Schema({
+//   userId: [
+//     {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "user",
+//     },
+//   ],
+//   orderNumber: Number,
+//   foods: [],
+//   totalPrice: Number,
+//   process: ["order", "complete"],
+//   createdDate: {
+//     type: Date,
+//     default: () => Date.now(),
+//   },
+//   district: String,
+//   khoroo: String,
+//   apartment: String,
+// });
+// const orderModel = mongoose.model("order", orderSchema);
+// export { orderModel };
