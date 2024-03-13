57cfea11a6b43af77c1988c2d8d03b8a9b5fc157
Orshikh <orshikh@Orshikhs-MacBook-Pro.local>
2024-03-13 16:41:33 +0800

    income front

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import GeneralInfo from "@/components/GeneralInfo";
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
@@ -21,25 +20,4 @@ const DashBoard = () => {
 };
 
 export default withAuth(DashBoard);
-// "use client"
-// import React, { useState } from 'react';
 
-
-// function App() {
-//   const [backgroundColor, setBackgroundColor] = useState('white');
-
-//   const changeBackground = () => {
-//     // Toggle between red and blue background color
-//     setBackgroundColor(prevColor => prevColor === 'red' ? 'blue' : 'red');
-//   };
-
-//   return (
-//     <div className="App" style={{ backgroundColor: backgroundColor }}>
-//       <div className="container">
-//         <button className="button" onClick={changeBackground}>Change Background</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
