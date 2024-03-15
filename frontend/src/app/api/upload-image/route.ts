618d385451fa842ca4212f31dc9dbf5fb1abe6c0
Бямбажаргал Батдэлгэр <23LP6131@G303-CC0156.local>
2024-03-15 10:33:52 +0800

    added s3 uploader

--- frontend/src/app/api/upload-image/route.ts
+++ frontend/src/app/api/upload-image/route.ts
@@ -6,8 +6,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const s3 = new S3Client({
-    region: 'ap-southeast-1',
-    // endpoint: process.env.ENDPOINT || '',
+    region: process.env.REGION,
     credentials: {
         accessKeyId: process.env.ACCESS_KEY_ID || '',
         secretAccessKey: process.env.SECRET_ACCESS_KEY || '',
