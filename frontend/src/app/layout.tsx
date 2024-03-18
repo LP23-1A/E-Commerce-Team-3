42dae758c04f9edc2f44b58d2e7ad6f8f63598c4
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-03-18 18:18:20 +0800

    statusaar ni ymarch aldaagu filterddeg bolson

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import { OrderFilterProvider } from "@/components/OrderByStatusProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <OrderFilterProvider>{children}</OrderFilterProvider>
+        </body>
       </AuthProvider>
     </html>
   );
--- frontend/src/components/StatusBar.tsx
+++ frontend/src/components/StatusBar.tsx
@@ -1,39 +1,63 @@
-'use client'
-import axios from 'axios'
-import { useState } from 'react'
-const StatusBar = ({ selectedOrderId,onClick }: any) => {
-  const [selected, setSelected] = useState('')
-  const status = ["Ordered", "Preparing to ship", "Shipped", "Delivered"]
+"use client";
+import axios from "axios";
+import { useState } from "react";
+const StatusBar = ({ selectedOrderId, onClick }: any) => {
+  const [selected, setSelected] = useState("");
+  const status = [
+    "Ordered",
+    "Preparing to ship",
+    "Shipped",
+    "Delivered",
+    "New Order",
+    "Cancelled",
+  ];
   const postHandler = async () => {
     try {
-      console.log(selected)
-      const res = await axios.put(`http://localhost:8000/order/${selectedOrderId}`, {
-        status: selected
-      })
-      onClick(onClick)
+      console.log(selected);
+      const res = await axios.put(
+        `http://localhost:8000/order/${selectedOrderId}`,
+        {
+          status: selected,
+        }
+      );
+      onClick(onClick);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   return (
-    <div className='bg-black bg-opacity-70 w-full h-full fixed top-0  z-10 flex justify-center items-center'>
-      <div  className='flex flex-col p-5  items-center w-[400px] bg-white rounded-lg'  >
-      <select className='p-3 border-2 border-solid rounded-md ' value={selected} onChange={(e) => { setSelected(e.target.value) }} >
-      {status.map((e) => (
-        <option className='p-2' value={e} key={e} >
-          {e}
-        </option>
-      ))}
-    </select>
-      <div className='flex gap-4 mt-3'>
-        <button className='bg-black text-white  px-6 py-3 rounded-lg' onClick={onClick}>Close</button>
-      <button className='bg-black text-white  px-6 py-3 rounded-lg' onClick={postHandler}>Save</button>
+    <div className="bg-black bg-opacity-70 w-full h-full fixed top-0  z-10 flex justify-center items-center">
+      <div className="flex flex-col p-5  items-center w-[400px] bg-white rounded-lg">
+        <select
+          className="p-3 border-2 border-solid rounded-md "
+          value={selected}
+          onChange={(e) => {
+            setSelected(e.target.value);
+          }}
+        >
+          {status.map((e) => (
+            <option className="p-2" value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+        <div className="flex gap-4 mt-3">
+          <button
+            className="bg-black text-white  px-6 py-3 rounded-lg"
+            onClick={onClick}
+          >
+            Close
+          </button>
+          <button
+            className="bg-black text-white  px-6 py-3 rounded-lg"
+            onClick={postHandler}
+          >
+            Save
+          </button>
+        </div>
       </div>
-      </div>
-
     </div>
+  );
+};
 
-  )   
-}
-
-export default StatusBar
+export default StatusBar;
