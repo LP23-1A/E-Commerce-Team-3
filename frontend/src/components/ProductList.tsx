463aa0d989435bfaf1a0c6db200debcf98ddc836
Бямбажаргал Батдэлгэр <23LP6131@G303-CC0156.local>
2024-03-18 12:32:24 +0800

    fixed delete product

--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -8,8 +8,8 @@ import { useRouter } from "next/navigation";
 
 const ProductList = () => {
 
-  const [selectedProduct , setselectedProduct] = useState("")
-
+  const [selectedProduct , setselectedProduct] = useState<string | null>(null);
+  
   const router = useRouter()
   const movepage = () => {
       router.push(`product`);
@@ -30,9 +30,9 @@ const ProductList = () => {
 
   const deleteHandler = async (productId:string) => {
     try { 
-      await axios.delete(`http://localhost:8000/product/${productId}`, {
-
+     const res = await axios.delete(`http://localhost:8000/product/${productId}`, {
       })
+     
     } catch (error) {
       console.log(error)
     }
@@ -60,10 +60,10 @@ const ProductList = () => {
             <p className="pr-10 text-sm text-[#3F4145]">2024.01.20</p>
             
             <div className="flex gap-2 items-center ">
-              <button {...e._id} onClick={()=>deleteHandler(e._id)} >
+              <button  onClick={()=>deleteHandler(e._id)} >
               <Delete  />
               </button>
-              <button  {...e._id = selectedProduct }  onClick={() => move(e._id)}>
+              <button    onClick={() => move(e._id)}>
               <Edit />
               </button>
             </div>
