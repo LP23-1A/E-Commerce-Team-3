62bba197f07f12936a8033a50a472df4a1fb4a6f
Orshikh <orshikh@Orshikhs-MacBook-Pro.local>
2024-03-27 08:20:47 +0800

    products cart

---
+++ frontend/src/components/userComponents/SavedProduct.tsx
@@ -0,0 +1,29 @@
+const SavedProduct = () => {
+  return (
+    <div>
+      <h3 className="text-[#151875] font-extrabold ">Хадгалсан бүтээгдэхүүн</h3>
+      <div>
+        <div>
+          <img src="" alt="" className="w-[270px] h-[270px]"/>
+          <div className="flex flex-col">
+            <div>
+              <span>usan ygaan sandal</span>
+            </div>
+            <div className="flex">
+              <p>750,000</p>
+              <p>(32)</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
+              est adipiscing in phasellus non in justo.
+            </p>
+            <div className="flex">
+              <button>Hudaldan awah</button>
+              <button>Hasah</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
