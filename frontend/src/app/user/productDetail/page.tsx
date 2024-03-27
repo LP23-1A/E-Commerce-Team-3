d0119a0b45c3feeb9b882f6459da98e2d336f9df
Bymbaabatdelger <baymba.0108@icloud.com>
2024-03-27 20:58:22 +0800

    added product detail page

---
+++ frontend/src/app/user/productDetail/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Topbar from '@/components/userComponents/Topbar';
+import { UserFooter } from '@/components/userComponents/Footer';
+import ProductDetail from '@/components/userComponents/ProductDetail';
+import AdditionalInformation from '@/components/userComponents/AddtionalInformation';
+
+const ProductDetailPage = () => {
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+
+        const productId = localStorage.getItem('productId');
+
+        const fetchProduct = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/product/${productId}`);
+                setProduct(response.data);
+            } catch (error) {
+                console.error('Error fetching product:', error);
+            }
+        };
+
+        if (productId) {
+            fetchProduct();
+        }
+        return () => {
+            localStorage.removeItem('productId');
+        };
+    }, []); 
+
+    if (!product) {
+        return <div>Loading product details...</div>;
+    }
+
+    return (
+        <div>
+         <Topbar/>
+         <ProductDetail productName={product.productName} price={product.price} description={product.description} image1={product.images[0]} image2={product.images[1]} image3={product.images[2]}/>
+         <AdditionalInformation category= {product.mainCategory.mainCategoryName} productName={product.productName}/>
+         <UserFooter/>
+        </div>
+    );
+};
+
+export default ProductDetailPage;
---
+++ frontend/src/components/userComponents/AddtionalInformation.tsx
@@ -0,0 +1,37 @@
+const AdditionalInformation = ({ productName}:any) => {
+    return(
+        <div className="px-[350px] py-20 bg-[#F9F8FE] flex flex-col" >
+            <div className="flex gap-6 py-6">
+                <p className="text-[#151875] font-bold text-[24px] ">Нэмэлт мэдээлэл</p>
+                <p  className="text-[#151875] font-bold text-[24px]">Үнэлгээ</p>
+            </div>
+            <div className="bg-[#fff] flex flex-col gap-4 py-[48px] px-[24px] rounded-[4px]">
+                <p className="text-[#151875] font-bold text-[22px]">Үзүүлэлтүүд</p>
+                <div className="flex gap-6">
+                    <div>
+                        <p>Загвар & Зориулалт</p>
+                        <p>Хүйс</p>
+                        <p>Насны ангилал</p>
+                        <p>Улирал</p>
+                        <p>Материал</p>
+                        <p>Ерөнхий өнгө</p>
+                        <p>Зориулалт</p>
+                    </div>
+                    <div>
+                        <p>{productName}</p>
+                        <p>Эмэгтэй </p>
+                        <p>Том хүн</p>
+                        <p>Бүх улирал</p>
+                        <p>Арьсан</p>
+                        <p>Хул</p>
+                        <p>Өдөр тутам</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AdditionalInformation
