0c712daf92ab09848b88d38940d043630d8f53dc
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-03-28 21:01:32 +0800

    ahiad 1

--- frontend/src/app/income/page.tsx
+++ frontend/src/app/income/page.tsx
@@ -1,26 +1,23 @@
 "use client";
+import useSWR from "swr";
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
-import useSWR from "swr";
 import formatDate from "@/components/utils/FormatDate";
 import OrderDayFilterInc from "@/components/OrderDayFilterInc";
 import { useInputOrderFilter } from "@/components/OrderFilterProvider";
 
 const Income = () => {
-  const backendPoint = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT
+  const backendPoint = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT;
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
-  const { data } = useSWR(
-    `${backendPoint}/order`,
-    fetcher
-  );
+  const { data } = useSWR(`${backendPoint}/order`, fetcher);
 
   const { filters } = useInputOrderFilter();
 
   const getTotalPrice = () => {
     let totalPrice = 0;
     if (data && data.length > 0) {
-      data.forEach((product:any) => {
-        const amount = product?.amountToPaid;
+      data.forEach((order: any) => {
+        const amount = order?.amountToPaid;
         if (!isNaN(amount)) {
           totalPrice += amount;
         }
@@ -32,32 +29,35 @@ const Income = () => {
   const week = new Date();
   week.setDate(week.getDate() - 7);
 
-  const month = new Date()
-  month.setDate(month.getDate() - 30)
+  const month = new Date();
+  month.setDate(month.getDate() - 30);
 
-  const filterByWeek = (order, filters, week) => {
+  const filterByWeek = (order: any, filters: any, week: Date) => {
     const orderDate = new Date(order.createdAt);
     return !filters.filterByWeek || orderDate.getTime() >= week.getTime();
   };
 
-  const filterByMonth = (order, filters, month) => {
+  const filterByMonth = (order: any, filters: any, month: Date) => {
     const orderDate = new Date(order.createdAt);
     return !filters.filterByMonth || orderDate.getTime() >= month.getTime();
   };
 
-  const filterByDay = (order, filters) => {
-    return !filters.filterByDay || formatDate(order.createdAt) === formatDate(new Date());
+  const filterByDay = (order: any, filters: any) => {
+    return (
+      !filters.filterByDay ||
+      formatDate(order.createdAt) === formatDate(new Date())
+    );
   };
 
   const filteredData = data
-  ? data.filter((order) => {
-    return (
-      filterByDay(order, filters) &&
-      filterByWeek(order, filters, week) &&
-      filterByMonth(order, filters, month)
-    );
-  })
-  : [];
+    ? data.filter((order: any) => {
+        return (
+          filterByDay(order, filters) &&
+          filterByWeek(order, filters, week) &&
+          filterByMonth(order, filters, month)
+        );
+      })
+    : [];
 
   return (
     <div className="w-screen h-screen bg-gray-200 justify-center items-center">
@@ -76,7 +76,7 @@ const Income = () => {
               <h2 className="text-3xl font-semibold text-[#121316]">
                 {getTotalPrice()}₮
               </h2>
-          <OrderDayFilterInc/>
+              <OrderDayFilterInc />
             </div>
           </div>
           <div className="flex flex-col items-center mt-5 ">
@@ -96,32 +96,28 @@ const Income = () => {
                 </p>
               </div>
               <div>
-              {filteredData &&
-  filteredData
-    .filter(order => order.amountToPaid) 
-    .map((e, index) => {
-        return(
-            <div className="flex justify-between py-2" key={index}>
-            <p className="text-xs font-normal text-[#3F4145] w-[150px] px-6">
-              #{e.orderNumber}
-            </p>
-            <div>
-              <p className="text-xs font-normal text-[#3F4145] w-[200px] px-6">
-                {e.userId.email}
-              </p>
-              <p className="text-xs font-normal text-[#3F4145] w-[200px] px-6">
-                {e.userId.phoneNumber}
-              </p>
-            </div>
-            <p className="text-xs font-normal text-[#3F4145] w-[110px] px-8">
-              {e.amountToPaid}₮
-            </p>
-            <p className="text-xs font-normal text-[#3F4145] w-[170px] px-6">
-              {formatDate(e.createdAt)}
-            </p>
-          </div>
-        )
-    })}
+                {filteredData &&
+                  filteredData.map((order: any, index: number) => (
+                    <div className="flex justify-between py-2" key={index}>
+                      <p className="text-xs font-normal text-[#3F4145] w-[150px] px-6">
+                        #{order.orderNumber}
+                      </p>
+                      <div>
+                        <p className="text-xs font-normal text-[#3F4145] w-[200px] px-6">
+                          {order.userId.email}
+                        </p>
+                        <p className="text-xs font-normal text-[#3F4145] w-[200px] px-6">
+                          {order.userId.phoneNumber}
+                        </p>
+                      </div>
+                      <p className="text-xs font-normal text-[#3F4145] w-[110px] px-8">
+                        {order.amountToPaid}₮
+                      </p>
+                      <p className="text-xs font-normal text-[#3F4145] w-[170px] px-6">
+                        {formatDate(order.createdAt)}
+                      </p>
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
