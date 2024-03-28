702e9ed9b0a5575907fb2275e07f78e196c58304
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-03-28 21:06:40 +0800

    ahiad 2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,11 +4,6 @@ import "./globals.css";
 import AuthProvider from "@/components/AuthProvider";
 import { OrderFilterProvider } from "@/components/OrderByStatusProvider";
 import { OrderInputFilterProvider } from "@/components/OrderFilterProvider";
-import { ProductFilterProvider } from "@/components/ProductFilterContext";
-import ProductList from "@/components/ProductList";
-import ProductFilterByCategory from "@/components/ProductFilterByCategory";
-import ProductFilterByPrice from "@/components/ProductFilterByPrice";
-import ProductFilterByMonth from "@/components/ProductFilterByMonth";
 import { BasketProvider } from "@/components/userComponents/OrderContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,16 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-
-
-      <body className={inter.className}>
-        <BasketProvider>
-          <OrderFilterProvider>
-        <OrderInputFilterProvider>
-        {children}
-        </OrderInputFilterProvider>
-        </OrderFilterProvider>
-        </BasketProvider>
+        <body className={inter.className}>
+          <BasketProvider>
+            <OrderFilterProvider>
+              <OrderInputFilterProvider>{children}</OrderInputFilterProvider>
+            </OrderFilterProvider>
+          </BasketProvider>
         </body>
       </AuthProvider>
     </html>
